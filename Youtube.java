406c16936520f077a466eaaeec71ba0e59a10b6f
Salraka <karla.ramirez.salud@gmail.com>
2024-02-16 10:57:39 -0600

    Updated

---
+++ Youtube.java
@@ -0,0 +1,15 @@
+public class Youtube {
+    public static void main(String[] args) {
+        Usuario usu1 = new Usuario("Alejandra");
+        Video vid1= new Video (9000,"Curso de ingles","https://youtube.com/k8");
+        usu1.SubirVideo(vid1);
+        usu1.MostrarInfoUsuario();
+        System.out.println("---");
+        
+        Usuario usu2 = new Usuario ("Fernanda");
+        Comentario commt1 = new Comentario("Excelente video. ", usu2);
+        
+        System.out.println("El usuario: " + usu2.getNombre() + "    Realizo el comentario: " + commt1.getContenido());
+               
+    }
+} 
